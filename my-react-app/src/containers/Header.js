51b84cad554c8971420b0bec4d34104d228279a3
Raymo Washington <raymowash100@gmail.com>
2021-04-23 13:39:56 -0600

    first commit

---
+++ my-react-app/src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Raymo's Shop</h2>
+            </div>
+        </div>
+    )
+
+
+};
+
+
+
+
+
+export default Header;
